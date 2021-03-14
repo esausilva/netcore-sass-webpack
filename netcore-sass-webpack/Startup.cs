7d9f2a99897176736b92772367f25c0d28971d86
Esau Silva <esausilva85@gmail.com>
2021-03-13 18:22:27 -0600

    Update to .NET 5, Webpack 5 and JS dependencies

--- netcore-sass-webpack/Startup.cs
+++ netcore-sass-webpack/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace netcore_sass_webpack
 {
@@ -21,18 +17,17 @@ namespace netcore_sass_webpack
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            services.AddControllersWithViews();
+
             services.Configure<CookiePolicyOptions>(options => {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             } else {
@@ -41,11 +36,12 @@ namespace netcore_sass_webpack
 
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseRouting();
 
-            app.UseMvc(routes => {
-                routes.MapRoute(
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
--- netcore-sass-webpack/webpack.config.js
+++ netcore-sass-webpack/webpack.config.js
@@ -1,34 +1,34 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const postcssPresetEnv = require('postcss-preset-env');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const postcssPresetEnv = require("postcss-preset-env");
 
 // We are getting 'process.env.NODE_ENV' from the NPM scripts
 // Remember the 'dev' script?
-const devMode = process.env.NODE_ENV !== 'production';
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
     // Tells Webpack which built-in optimizations to use
     // If you leave this out, Webpack will default to 'production'
-    mode: devMode ? 'development' : 'production',
+    mode: devMode ? "development" : "production",
 
     // Webpack needs to know where to start the bundling process,
     // so we define the Sass file under './Styles' directory
-    entry: ['./Styles/site.scss'],
+    entry: ["./Styles/site.scss"],
 
     // This is where we define the path where Webpack will place
     // a bundled JS file. Webpack needs to produce this file,
     // but for our purposes you can ignore it
     output: {
-        path: path.resolve(__dirname, 'wwwroot'),
+        path: path.resolve(__dirname, "wwwroot"),
 
         // Specify the base path for all the styles within your
         // application. This is relative to the output path, so in
         // our case it will be ./wwwroot/css
-        publicPath: '/css',
+        publicPath: "/css",
 
         // The name of the output bundle. Path is also relative
         // to the output path, so './wwwroot/js'
-        filename: 'js/sass.js'
+        filename: "js/sass.js",
     },
     module: {
         // Array of rules that tells Webpack how the modules (output)
@@ -45,47 +45,47 @@ module.exports = {
                     {
                         // Extracts the CSS into a separate file and uses the
                         // defined configurations in the 'plugins' section
-                        loader: MiniCssExtractPlugin.loader
+                        loader: MiniCssExtractPlugin.loader,
                     },
                     {
                         // Interprets CSS
-                        loader: 'css-loader',
+                        loader: "css-loader",
                         options: {
-                            importLoaders: 2
-                        }
+                            importLoaders: 2,
+                        },
                     },
                     {
                         // Use PostCSS to minify and autoprefix with vendor rules
-	                    // for older browser compatibility
-                        loader: 'postcss-loader', 
+                        // for older browser compatibility
+                        loader: "postcss-loader",
                         options: {
-                            ident: 'postcss',
-
-                            // We instruct PostCSS to autoprefix and minimize our 
-                            // CSS when in production mode, otherwise don't do 
+                            // We instruct PostCSS to autoprefix and minimize our
+                            // CSS when in production mode, otherwise don't do
                             // anything.
-                            plugins: devMode
-                                ? () => []
-                                : () => [
-                                    postcssPresetEnv({
-                                        // Compile our CSS code to support browsers 
-                                        // that are used in more than 1% of the 
-                                        // global market browser share. You can modify 
-                                        // the target browsers according to your needs 
-                                        // by using supported queries.
-                                        // https://github.com/browserslist/browserslist#queries
-                                        browsers: ['>1%']
-                                    }),
-                                    require('cssnano')()
-                                ]
-                        }
+                            postcssOptions: {
+                                plugins: devMode
+                                    ? () => []
+                                    : () => [
+                                        postcssPresetEnv({
+                                            // Compile our CSS code to support browsers
+                                            // that are used in more than 1% of the
+                                            // global market browser share. You can modify
+                                            // the target browsers according to your needs
+                                            // by using supported queries.
+                                            // https://github.com/browserslist/browserslist#queries
+                                            browsers: [">1%"],
+                                        }),
+                                        require("cssnano")(),
+                                    ],
+                            },
+                        },
                     },
                     {
                         // Adds support for Sass files, if using Less, then
                         // use the less-loader
-                        loader: 'sass-loader'
-                    }
-                ]
+                        loader: "sass-loader",
+                    },
+                ],
             },
             {
                 // Adds support to load images in your CSS rules. It looks for
@@ -93,11 +93,11 @@ module.exports = {
                 test: /\.(png|jpe?g|gif)$/,
                 use: [
                     {
-                        loader: 'file-loader',
+                        loader: "file-loader",
                         options: {
                             // The image will be named with the original name and
                             // extension
-                            name: '[name].[ext]',
+                            name: "[name].[ext]",
 
                             // Indicates where the images are stored and will use
                             // this path when generating the CSS files.
@@ -106,23 +106,23 @@ module.exports = {
                             // the CSS file, file-loader will output as
                             // url(../images/pattern.png), which is relative
                             // to '/css/site.css'
-                            publicPath: '../images',
+                            publicPath: "../images",
 
                             // When this option is 'true', the loader will emit the
                             // image to output.path
-                            emitFile: false
-                        }
-                    }
-                ]
-            }
-        ]
+                            emitFile: false,
+                        },
+                    },
+                ],
+            },
+        ],
     },
     plugins: [
         // Configuration options for MiniCssExtractPlugin. Here I'm only
         // indicating what the CSS output file name should be and
         // the location
         new MiniCssExtractPlugin({
-            filename: devMode ? 'css/site.css' : 'css/site.min.css'
-        })
-    ]
+            filename: devMode ? "css/site.css" : "css/site.min.css",
+        }),
+    ],
 };
